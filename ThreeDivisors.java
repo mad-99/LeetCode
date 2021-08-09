191dbf43d794c752576b5a7685c687614d2ca025
mad-99 <72245519+mad-99@users.noreply.github.com>
2021-08-09 23:31:25 +0530

    Update ThreeDivisors.java

--- ThreeDivisors.java
+++ ThreeDivisors.java
@@ -30,3 +30,23 @@ int i=1,count=0;
         return((count==3) ?  true : false);
 }
 
+// kotlin code .............................................
+
+class Solution {
+    fun isThree(n: Int): Boolean {
+        var i=1;
+        var count=0;
+        while(n>=i){
+            if(n%i==0)
+                count++;
+            i++;
+        }
+        if(count==3){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+

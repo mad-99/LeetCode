e8a1dab6ad64fb046e1ec92a721bb119640b624b
mad-99 <madhurmaheshwari1999@gmail.com>
2021-08-25 23:10:28 +0530

    leet

---
+++ ClimbingStairs.c
@@ -0,0 +1,15 @@
+
+
+int climbStairs(int n){
+if(n == 1 || n ==2 ) return n;
+        
+        int a = 1;
+        int b = 2;
+        
+        for(int i=3;i<=n;i++)
+        {
+            b = a+b;
+            a = abs(a-b);
+        }
+        return b;
+}

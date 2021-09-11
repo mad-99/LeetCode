c7c368ff3f7c0c5370161681c1735a85b0ace5f1
mad-99 <72245519+mad-99@users.noreply.github.com>
2021-09-11 21:08:23 +0530

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -7,13 +7,13 @@ class Solution {
             if(nums[mid] == target){
                 return mid;
             }
-        
-            if(nums[mid] < target){
+         if(nums[mid] < target){
                 left = mid + 1;
             }else{
                 right = mid - 1;
             }
            }
           return left;
+// Madhur
     }
 }
